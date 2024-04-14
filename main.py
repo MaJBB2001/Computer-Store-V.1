a70774158f8bd0a462b74de574cb04c989b294c5
MaJBB2001 <mariajesusbrenes4@gmail.com>
2024-04-14 15:45:01 -0600

    Crear Menú Principal

--- main.py
+++ main.py
@@ -1,16 +1,60 @@
- git # This is a sample Python script.
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def MostrarMenu():
 
+    print("*" * 35)
+    print("___________BIENVENIDOS_________")
+    print("________ Cumputer Store________")
+    print("*" * 35)
+    print("\nSeleccione una opción:\n")
+    print("1. Ver inventario")
+    print("2. Modificar inventario")
+    print("3. Agregar artículo")
+    print("4. Eliminar artículo")
+    print("5. Facturar Venta")
+    print("6. Clientes")
+    print("7. Guardar")
+    print("8. Salir")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
 
+    while True:
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        MostrarMenu()
+        opcion = input("\nSeleccione una opción: ")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        if opcion == "1":
+            print("-" * 30)
+            print("*** Inciando Inventario ***")
+            print("-" * 30)
+        elif opcion == "2":
+            print("-" * 30)
+            print("*** Inciando Modificar ***")
+            print("-" * 30)
+        elif opcion == "3":
+            print("-" * 30)
+            print("*** Inciando Agregar***")
+            print("-" * 30)
+        elif opcion == "4":
+            print("-" * 30)
+            print("*** Inciando Eliminar***")
+            print("-" * 30)
+        elif opcion == "5":
+            print("-" * 30)
+            print("*** Inciando Guardar ***")
+            print("-" * 30)
+        elif opcion == "6":
+            print("-" * 30)
+            print("*** Inciando Guardar ***")
+            print("-" * 30)
+        elif opcion == "7":
+            print("-" * 30)
+            print("*** Inciando Guardar ***")
+            print("-" * 30)
+        elif opcion == "8":
+            input("Saliendo del programa. ¡Hasta luego!")
+            break
+        else:
+            print("Opción inválida. Por favor, seleccione una opción válida.")
+            input("Presione Enter para continuar...")
+if __name__ == "__main__":
+    main()
